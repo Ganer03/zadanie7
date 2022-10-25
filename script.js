969c711cb21e3eae61599c6e761c03c5ff397b0a
Ganer03 <112845740+Ganer03@users.noreply.github.com>
2022-10-26 01:16:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const sliderLine=document.querySelector('.container .slider .slider-line');
 
 document.querySelector('.container .buttons .next').addEventListener('click', function() {
     offset += 300;
-    if (offset > 4500) {
+    if (offset > 3800) {
         offset = 0;
     }
     sliderLine.style.left = -offset + 'px';
@@ -12,7 +12,7 @@ document.querySelector('.container .buttons .next').addEventListener('click', fu
 document.querySelector('.container .buttons .prev').addEventListener('click', function() {
     offset -= 300;
     if (offset < 0) {
-        offset = 4500;
+        offset = 3800;
     }
     sliderLine.style.left = -offset + 'px';
 })
